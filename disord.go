e6132ec4da4cff914fdcaaf0a1c6bf2c89cdd85d
Samuel Hernandez <sam@samhza.com>
2021-04-21 22:01:19 -0400

    add readline-like input with x/term

--- disord.go
+++ disord.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
@@ -11,12 +12,16 @@ import (
 	"github.com/diamondburned/arikawa/v2/discord"
 	"github.com/diamondburned/arikawa/v2/gateway"
 	"github.com/diamondburned/arikawa/v2/state"
+	"golang.org/x/term"
 )
 
 var client struct {
 	s  *state.State
 	ch discord.ChannelID
 }
+
+var out io.Writer
+var oldState *term.State
 
 func main() {
 	var err error
@@ -28,47 +33,63 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	sc := bufio.NewScanner(os.Stdin)
 	input := make(chan string)
-	events, _ := client.s.ChanFor(func(_ interface{}) bool { return true })
-	go func() {
-		for sc.Scan() {
-			input <- sc.Text()
+	errs := make(chan error)
+	if term.IsTerminal(int(os.Stdin.Fd())) {
+		oldState, err = term.MakeRaw(int(os.Stdin.Fd()))
+		if err != nil {
+			log.Fatalln(err)
 		}
-	}()
+		defer term.Restore(int(os.Stdin.Fd()), oldState)
+		c := struct {
+			io.Reader
+			io.Writer
+		}{os.Stdin, os.Stdout}
+		term := term.NewTerminal(c, "→ ")
+		out = term
+		log.SetOutput(term)
+		go terminalInput(term, input, errs)
+	} else {
+		out = os.Stdout
+		go stdinInput(input, errs)
+	}
+	events, _ := client.s.ChanFor(func(_ interface{}) bool { return true })
 	for {
 		select {
 		case line := <-input:
 			handleInput(line)
 		case ev := <-events:
 			handleEvent(ev)
+		case err = <-errs:
+			log.Fatalln(err)
 		}
 	}
 }
 
-func handleInput(line string) {
-	if line == ":q" || line == "\x05" {
-		return
+func stdinInput(input chan string, errs chan error) {
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		input <- sc.Text()
 	}
+	errs <- sc.Err()
+}
+
+func terminalInput(term *term.Terminal, input chan string, errs chan error) {
+	for {
+		line, err := term.ReadLine()
+		if err != nil {
+			errs <- err
+			break
+		}
+		input <- line
+	}
+}
+
+func handleInput(line string) {
 	if strings.HasPrefix(line, ":") {
 		splat := strings.Fields(line)
-		cmd := line[1]
-		switch cmd {
-		case 'c':
-			n, err := strconv.ParseInt(splat[1], 10, 64)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			chid := discord.ChannelID(n)
-			ch, err := client.s.Channel(chid)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			fmt.Println("join #" + ch.Name)
-			client.ch = discord.ChannelID(n)
-		}
+		cmd := splat[0][1:]
+		handleCommand(cmd, splat[1:])
 		return
 	}
 	_, err := client.s.SendText(client.ch, line)
@@ -77,19 +98,39 @@ func handleInput(line string) {
 	}
 }
 
+func handleCommand(cmd string, args []string) {
+	switch cmd {
+	case "c":
+		n, err := strconv.ParseInt(args[0], 10, 64)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		chid := discord.ChannelID(n)
+		ch, err := client.s.Channel(chid)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Fprintln(out, "join #"+ch.Name)
+		client.ch = discord.ChannelID(n)
+	}
+	return
+}
+
 func handleEvent(ev interface{}) {
 	switch ev := ev.(type) {
 	case *gateway.MessageCreateEvent:
 		if ev.ChannelID != client.ch {
 			return
 		}
-		printMsg(m)
+		printMsg(ev.Message)
 	}
 }
 
 func printMsg(m discord.Message) {
 	lines := strings.Split(m.Content, "\n")
 	for _, line := range lines {
-		fmt.Printf("%s → %s\n", m.Author.Username, line)
+		fmt.Fprintf(out, "%s → %s\n", m.Author.Username, line)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module git.sr.ht/~samhza/disord
 
 go 1.16
 
-require github.com/diamondburned/arikawa/v2 v2.0.6
+require (
+	github.com/diamondburned/arikawa/v2 v2.0.6
+	golang.org/x/term v0.0.0-20210406210042-72f3dc4e9b72
+)
